3a248d7c55c9a4e4a70e52ede4d4237a0c2ac9bc
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-12-17 08:06:21 +0800

    add: icon footer

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,20 @@
+"use client"
 import { SearchIcon } from "lucide-react";
 import Link from "next/link";
 import Logo from "./ui/Logo";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between p-5">
-      <div className="">
-        <Link href={"/"}>
-          <Logo />
-        </Link>
-      </div>
-      <div>
-        <SearchIcon />
+    <div className=" p-5">
+      <div className="flex justify-between">
+        <div className="">
+          <Link href={"/"}>
+            <Logo />
+          </Link>
+        </div>
+        <div>
+          <SearchIcon />
+        </div>
       </div>
     </div>
   );
--- app/components/_components/MobileFooter.tsx
+++ app/components/_components/MobileFooter.tsx
@@ -1,24 +1,32 @@
-import { Home, Info, Phone } from "lucide-react";
+"use client"
+import { Home, Info, PersonStanding, Phone } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const MobileFooter = () => {
+  const router = useRouter();
   const list = [
     {
       name: "Info",
       icon: Home,
+      link: "/"
     },
     {
       name: "Contact",
       icon: Phone,
+      link: "/contact"
     },
     {
-      name: "Info",
+      name: "Tentang Kami",
       icon: Info,
+      link: "/tentang-kami"
     },
     {
-      name: "Info",
-      icon: Info,
+      name: "Bergabung bersama kami",
+      icon: PersonStanding,
+      link: "/bergabung"
     },
   ];
+
   return (
     <div className="bg-transparent fixed flex justify-center p-2 w-full bottom-0 right-0 z-[9999] h-[70px]">
       <div className=" bg-green-400 rounded-full w-full h-full grid grid-cols-4 shadow-2xl">
@@ -26,7 +34,7 @@ const MobileFooter = () => {
           const Icon = item.icon;
           return (
             <div key={index} className="flex justify-center items-center">
-              <button>
+              <button onClick={() => router.push(`${item.link}`)}>
                 <Icon size={30} />
               </button>
             </div>
